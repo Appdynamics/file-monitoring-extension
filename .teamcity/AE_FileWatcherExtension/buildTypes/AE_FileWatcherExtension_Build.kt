6885a235f4ab360dbec4f3786bcf911411964b61
aditya.jagtiani <aditya.jagtiani@appdynamics.com>
2020-04-28 13:06:01 -0700

    Adding a missing import for nightly builds

--- .teamcity/AE_FileWatcherExtension/buildTypes/AE_FileWatcherExtension_Build.kt
+++ .teamcity/AE_FileWatcherExtension/buildTypes/AE_FileWatcherExtension_Build.kt
@@ -6,6 +6,7 @@ import AE_FileWatcherExtension.withDefaults
 import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.schedule
 
 object AE_FileWatcherExtension_Build : BuildType({
     uuid = "0d57ff72-2662-4184-9564-b5009a9fd7af"
